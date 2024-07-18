2d8b629fb4ed7b238e4ed67e534439c887ba9d65
Joao Ponte <joaop@hayesdavidson.com>
2024-07-18 14:54:05 +0100

    Implement Firebase Storage.

--- HDBook/AppDelegate.swift
+++ HDBook/AppDelegate.swift
@@ -5,4 +5,46 @@
 //  Created by hayesdavidson on 18/07/2024.
 //
 
-import Foundation
+import UIKit
+import SwiftUI
+import FirebaseCore
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        guard FirebaseApp.app() != nil else {
+            return false
+        }
+        deleteMissingFiles()
+
+        let coordinator = ARViewCoordinator(firebaseStorageService: FirebaseStorageService.shared)
+        let contentView = LaunchScreen(viewModel: TutorialCardsViewModel())
+            .environmentObject(coordinator)
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIHostingController(rootView: contentView)
+        self.window = window
+        window.makeKeyAndVisible()
+
+        return true
+    }
+
+    func deleteMissingFiles() {
+        Task {
+            do {
+                await FirebaseStorageService.shared.deleteMissingLocalFiles()
+            } catch {
+                print("Error during deletion of missing local files: \(error)")
+            }
+        }
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+}
