2bfad6cb34ec147e38d74051454ccde403bf3c5f
Joao Ponte <joaop@hayesdavidson.com>
2024-07-18 12:39:35 +0100

    Initial Commit

---
+++ HDBook/HDBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDBookApp.swift
+//  HDBook
+//
+//  Created by hayesdavidson on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HDBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
