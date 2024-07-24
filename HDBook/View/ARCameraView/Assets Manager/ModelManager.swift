a03ba4b0733a2350a6473b443922827f027248e3
Joao Ponte <joaop@hayesdavidson.com>
2024-07-24 17:27:52 +0100

    Create ModelManager and Get local 3DModel.

--- HDBook/View/ARCameraView/Assets Manager/ModelManager.swift
+++ HDBook/View/ARCameraView/Assets Manager/ModelManager.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import RealityKit
+
+class ModelManager: ModelManagement {
+    func loadModel(named modelName: String) async throws -> ModelEntity {
+        guard let modelURL = FirebaseStorageService.shared.getLocalModelURL(for: modelName) else {
+            throw NSError(domain: "ModelManager", code: 404, userInfo: [NSLocalizedDescriptionKey: "Model not found"])
+        }
+        let entity = try await ModelEntity.loadModel(contentsOf: modelURL)
+        return entity
+    }
+}
