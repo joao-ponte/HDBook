6bb86b1a82b2c260526223dc31d8a8f1208dc260
Joao Ponte <joaop@hayesdavidson.com>
2024-07-24 15:24:05 +0100

    Create ARProtocols

--- HDBook/View/ARCameraView/Assets Manager/ARProtocols.swift
+++ HDBook/View/ARCameraView/Assets Manager/ARProtocols.swift
@@ -5,4 +5,34 @@
 //  Created by hayesdavidson on 24/07/2024.
 //
 
-import Foundation
+import ARKit
+import RealityKit
+
+protocol ARSessionManagement {
+    func configureARView(_ arView: ARView) async
+    func resumeARSession() async
+    func pauseARSession()
+    func removeAllAnchors() async
+}
+
+protocol ARImageHandling {
+    func handleImageAnchor(_ imageAnchor: ARImageAnchor)
+    func placeImage360Screen(panoramaView: CTPanoramaView, imageAnchor: ARImageAnchor)
+}
+
+protocol ARVideoHandling {
+    func placeVideoScreen(videoScreen: ModelEntity, imageAnchor: ARImageAnchor, uuid: UUID)
+    func handleTrackingTimeout(for uuid: UUID)
+    func startTrackingTimer(for uuid: UUID)
+}
+
+protocol VideoManagement {
+    var videoPlayers: [UUID: AVPlayer] { get set }
+    func createVideoScreen(width: Float, height: Float, url: URL, uuid: UUID) -> ModelEntity
+    func createVideoItem(with url: URL) -> AVPlayerItem
+    func createVideoMaterial(with videoItem: AVPlayerItem, uuid: UUID) -> VideoMaterial
+}
+
+protocol ImageManagement {
+    func createPanoramaView(for image360URL: URL, frame: CGRect) -> CTPanoramaView?
+}
