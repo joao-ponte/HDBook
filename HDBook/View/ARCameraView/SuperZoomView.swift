8f2532f6e4b38a6af43ea1207546df708baa6a5b
Joao Ponte <joaop@hayesdavidson.com>
2024-08-02 14:43:25 +0100

    Add assets.

---
+++ HDBook/View/ARCameraView/SuperZoomView.swift
@@ -0,0 +1,18 @@
+//
+//  SuperZoomView.swift
+//  HDBook
+//
+//  Created by hayesdavidson on 02/08/2024.
+//
+
+import SwiftUI
+
+struct SuperZoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuperZoomView()
+}
