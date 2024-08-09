60263edb0e0c79f517336208808f1ade59f2c118
Joao Ponte <joaop@hayesdavidson.com>
2024-08-09 15:15:38 +0100

    Fix bugs.

---
+++ HDBook/View/ARCameraView/ImageZoom/SuperZoomView.swift
@@ -0,0 +1,58 @@
+//
+//  SuperZoomView.swift
+//  HDBook
+//
+//  Created by hayesdavidson on 09/08/2024.
+//
+
+import SwiftUI
+
+struct SuperZoomView: View {
+    let imageURL: URL
+    @EnvironmentObject var coordinator: ARViewCoordinator
+    @State private var uiImage: UIImage? = nil
+    @State private var zoomState: ZoomState = .min
+
+    var body: some View {
+        GeometryReader { geometry in
+            if let uiImage = uiImage {
+                ImageZoomView(
+                    proxy: geometry,
+                    isInteractive: .constant(true),
+                    zoomState: $zoomState,
+                    maximumZoomScale: 4.0,
+                    content: UIImageView(image: uiImage)
+                )
+                .overlay(
+                    Button(action: {
+                        coordinator.exitSuperZoomView()
+                    }) {
+                        Image(systemName: "arrow.backward.circle")
+                            .foregroundColor(.green)
+                            .font(.system(size: 28))
+                            .padding()
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                )
+                .edgesIgnoringSafeArea(.all)
+            } else {
+                ProgressView()
+                    .onAppear {
+                        loadImage()
+                    }
+            }
+        }
+    }
+
+    private func loadImage() {
+        Task {
+            do {
+                let data = try Data(contentsOf: imageURL)
+                self.uiImage = UIImage(data: data)
+            } catch {
+                print("Failed to load image: \(error)")
+            }
+        }
+    }
+}
+
