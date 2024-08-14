2c5b6f953b07ee1d6ea2f20f0d4f1daf9d30e0eb
Joao Ponte <joaop@hayesdavidson.com>
2024-08-14 16:37:48 +0100

    Add constants.

--- HDBook/Helpers/Constants.swift
+++ HDBook/Helpers/Constants.swift
@@ -13,20 +13,24 @@ struct Constants {
     static let modelsDirectory = "3DModels"
     static let arImagesDirectory = "AR Images"
     static let superZoomDirectory = "SuperZoom"
+    static let filmsDirectory = "Films"
     
     static let videoExtension = "mp4"
     static let image360Extension = "jpg"
     static let modelExtension = "usdz"
     static let superZoomExtension = "jpg"
+    static let filmExtension = "mp4"
     
     static let videoSuffix = "_CIN"
     static let image360Suffix = "_360"
     static let modelSuffix = "_ARM"
     static let superZoomSuffix = "_SPZ"
+    static let filmSuffix = "_FLM"
     
     static let assetTypeVideo = "video"
     static let assetTypeImage360 = "image360"
     static let assetTypeModel = "model"
     static let assetTypeSuperZoom = "superZoom"
+    static let filmSuffix = "_FLM"
 }
 
