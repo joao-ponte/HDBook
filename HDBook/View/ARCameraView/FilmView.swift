05cf27df9686f7045944a6afbb386d0f78c620cc
Joao Ponte <joaop@hayesdavidson.com>
2024-08-20 16:09:16 +0100

    Remove back arrow when in landscape.

--- HDBook/View/ARCameraView/FilmView.swift
+++ HDBook/View/ARCameraView/FilmView.swift
@@ -12,25 +12,37 @@ struct FilmView: View {
     let filmURL: URL
     @EnvironmentObject var coordinator: ARViewCoordinator
     @State private var player: AVPlayer?
+    @Environment(\.verticalSizeClass) var verticalSizeClass
 
     var body: some View {
         GeometryReader { geometry in
             if let player = player {
                 VideoPlayer(player: player)
                     .overlay(
-                        Button(action: {
-                            coordinator.exitFilmView()
-                        }) {
-                            Image(systemName: "arrow.backward.circle")
-                                .foregroundColor(.green)
-                                .font(.system(size: 28))
-                                .padding()
+                        verticalSizeClass == .compact ? nil :
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                coordinator.exitFilmView()
+                            }) {
+                                Image(systemName: "xmark.circle")
+                                    .foregroundColor(.green)
+                                    .font(.system(size: 28))
+                                    .padding()
+                            }
                         }
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                        .padding(.top, geometry.safeAreaInsets.top + 40)
-                        .padding(.leading, geometry.safeAreaInsets.leading)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                        .padding(.top, geometry.safeAreaInsets.top + 20)
+                        .padding(.trailing, geometry.safeAreaInsets.trailing + 20)
                     )
                     .edgesIgnoringSafeArea(.all)
+                    .gesture(
+                        DragGesture().onEnded { value in
+                            if value.translation.width > 100 || value.translation.height > 100 {
+                                coordinator.exitFilmView()
+                            }
+                        }
+                    )
                     .onAppear {
                         player.play()
                     }
